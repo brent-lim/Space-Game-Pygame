e174d5124ab59ec8aefa1128c8a6d53459a3110e
Jeamie Chan <jeamiechan@Jeamies-iMac.local>
2021-04-28 11:51:58 +0800

    Powerup, restart, enemy dash, game over

--- first_game.py
+++ first_game.py
@@ -1,5 +1,3 @@
-
-
 import pygame
 import random
 import math as m
@@ -10,18 +8,39 @@ def main():
     TRANSPARENT = (0,0,0,0)
     OPAQUE = (255,0,0,255)
     RUNNING = True
+    TOTAL_SCORE = 0
 
 # SCREEN DISPLAY SIZE, TITLE, AND OTHERS
     class Screen():
+        text_font = "freesansbold.ttf"
         screen = pygame.display.set_mode((800,600))
-        reload_font = pygame.font.Font("freesansbold.ttf",50)
+        reload_font = pygame.font.Font(f"{text_font}",50)
         reload_text = reload_font.render("Out of ammo", True, (255,255,255))
+        score_font = pygame.font.Font(f"{text_font}",50)
+        game_over_font = pygame.font.Font(f"{text_font}",50)
+        game_over_text = game_over_font.render("Game Over!",True,(255,255,255))
+        restart_font = pygame.font.Font(f"{text_font}",15)
+        restart_text = restart_font.render("Press escape to restart",True, (255,255,255))
 
         def set_screen_caption(self):
             pygame.display.set_caption("My first Pygame")
 
         def display_reload_text(self, x,y):
             self.screen.blit(self.reload_text, (x,y))
+
+        def display_score(self,x,y):
+            score_text = self.reload_font.render(str(TOTAL_SCORE),True,(255,255,255))
+            self.screen.blit(score_text,(x,y))
+        
+        def display_restart(self,x,y):
+            self.screen.blit(self.game_over_text,(x,y))
+            self.screen.blit(self.restart_text,(x + 60 ,y + 55))
+            
+
+
+   
+       
+        
     screen_init = Screen()
     screen_init.set_screen_caption
 
@@ -32,7 +51,7 @@ def main():
             self.character_x = 375
             self.character_y = 500
             self.character_x_change = 0
-
+            self.is_character_dead = False
 
     # TO DISPLAY THE CHARACTER
 
@@ -76,7 +95,7 @@ def main():
             self.enemy_y = y
             self.enemy_y_change = y_change
             self.explosion_img = pygame.image.load("assets/explosion56.png")
-
+            self.is_enemy_killed = False
 
     # TO DISPLAY THE ENEMY
 
@@ -110,6 +129,7 @@ def main():
             self.powerup_x = random.randrange(100,700)
             self.powerup_y = 510
             self.powerup_img = pygame.image.load("assets/gunpowerup50.png")
+            self.is_powerup = False
         def display_powerup(self,x,y):
             screen_init.screen.blit(self.powerup_img,(x,y))
     powerup_init = PowerUp()
@@ -132,17 +152,18 @@ def main():
         else:
             return False
 
-# TO MAKE SURE GAME IS QUITTABLE (while loop)
+# WHILE LOOP
 
     while RUNNING == True:
         milliseconds = pygame.time.get_ticks() - start_timer
         seconds = milliseconds / 1000
-        print(seconds)
+        
+        # print(seconds)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
  
-# KEYBINDS AND FUNCTIONS OF EACH 
+# KEYBINDS
             
             if event.type == pygame.KEYDOWN:
 
@@ -159,7 +180,9 @@ def main():
     # OTHER FUNCTIONS KEYS
 
                 if event.key == pygame.K_SPACE:
-                        bullet_init.bullet_y_change = -5
+                    bullet_init.bullet_y_change = -5
+                if event.key == pygame.K_ESCAPE:
+                    main()
 
             if event.type == pygame.KEYUP:
 
@@ -173,7 +196,9 @@ def main():
 
 # COLLISION AND EXPLOSIONS (almost done check note)
 
+        
         for enemy in enemy_array:
+            enemy.enemy_y += enemy.enemy_y_change
             collision_detect = is_collided(enemy.enemy_x,enemy.enemy_y,bullet_init.bullet_x,bullet_init.bullet_y)
             enemy.enemy_img.fill(TRANSPARENT)
             if seconds >= 5:
@@ -182,10 +207,31 @@ def main():
                 bullet_init.bullet_x = character_init.character_x + 10
                 bullet_init.bullet_y = character_init.character_y + 10
                 enemy.enemy_img.fill(TRANSPARENT)
-                
                 enemy.display_explosion(enemy.enemy_x,enemy.enemy_y)
+                TOTAL_SCORE += 100
+                enemy_array.remove(enemy)
+                enemy.is_enemy_killed = True
+
+            if seconds >= 7 and seconds <= 8:
+                if enemy.is_enemy_killed == False:
+                    enemy.enemy_y_change = 5
+
                 
-                enemy.enemy_y = -1000
+                    if is_collided(enemy.enemy_x,enemy.enemy_y,character_init.character_x,character_init.character_y) == True:
+                        enemy.display_explosion(character_init.character_x,character_init.character_y)
+                        character_init.character_img.fill(TRANSPARENT)
+                        bullet_init.bullet_img.fill(TRANSPARENT)
+                        
+                        enemy_array.remove(enemy)
+                        enemy.enemy_y_change = 0
+                        character_init.is_character_dead = True
+                        enemy.is_enemy_killed = True
+                    if enemy.enemy_y == 650:
+                        enemy_array.remove(enemy)
+                    
+
+        if character_init.is_character_dead == True:
+            screen_init.display_restart(250,300)
                 
 # VALUES THAT ARE UNDECLARABLE AT THE START 
 
@@ -194,53 +240,55 @@ def main():
         bullet_init.bullet_x += bullet_init.bullet_x_change
         bullet_init.bullet_y += bullet_init.bullet_y_change
 
-        if bullet_init.bullet_y == 0:
+        if bullet_init.bullet_y == -10:
             bullet_init.bullet_x = character_init.character_x + 10
             bullet_init.bullet_y = character_init.character_y + 10
 
-# BULLET, SHIP, AND ENEMY DISPLAY (not done with extra bullet powerup display)
+# BULLET, SHIP, AND ENEMY DISPLAY 
 
         bullet_init.display_bullet(bullet_init.bullet_x,bullet_init.bullet_y)
 
-        
         character_init.display_character(character_init.character_x,character_init.character_y)
         for enemy in enemy_array:
             enemy.display_enemy(enemy.enemy_x, enemy.enemy_y)
 
+        screen_init.display_score(55,55)
 
     # POWERUPS
 
-        if seconds >= 2:
-            powerup_init.display_powerup(powerup_init.powerup_x,powerup_init.powerup_y)
+        if seconds >= 10:
+            if character_init.is_character_dead == True:
+                pass
+            else:
+                powerup_init.display_powerup(powerup_init.powerup_x,powerup_init.powerup_y)
+            
+
             if is_collided(character_init.character_x,character_init.character_y,powerup_init.powerup_x,powerup_init.powerup_y):
+                powerup_init.is_powerup = True
                 powerup_init.powerup_img.fill(TRANSPARENT)
-                bullet_init.display_bullet(bullet_init.bullet_x + 15,bullet_init.bullet_y + 10)
-                bullet_init.display_bullet(bullet_init.bullet_x - 13,bullet_init.bullet_y +10 )
+            
+        
+        if powerup_init.is_powerup == True:
+            bullet_init.display_bullet(bullet_init.bullet_x + 15,bullet_init.bullet_y + 10)
+            bullet_init.display_bullet(bullet_init.bullet_x - 13,bullet_init.bullet_y +10 )
 
     # UPDATING THE DISPLAY SCREEN
         pygame.display.update()
 
 
-
-
-
-        
-
-
 main()
 
 
-# master main
 
 
 
-# ideas
-# when character x = certain number like when the player hovers over
-
+# ----------------------------------------------------------------------------------------------------------------------
 # note
 # when seconds is greater than or equal to 5 then the boxes will appear 
 # based on https://nerdparadise.com/programming/pygameblitopacity there will be a box which there is 
 # so follow instructions and rename when necessary
+
+
 
 
 
